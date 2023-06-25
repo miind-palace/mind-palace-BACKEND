b683d3408eb19f5a5d4e35717b3d4a999720efcf
Dev_Owon <chlehdnjs@gmail.com>
2023-06-26 04:10:52 +0900

    feat: feat cors webconfig

---
+++ src/main/java/com/mindpalace/MP_Backend/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.mindpalace.MP_Backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("mind-palace-fe.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
