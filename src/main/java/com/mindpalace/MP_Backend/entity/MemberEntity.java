480b53c9a150afe22501ed2d90b274e7ae14e15f
reheat <reheat1540@gmail.com>
2023-10-08 22:49:23 +0900

    feat : 비밀번호 제약조건 추가(8글자 이상, 영문·숫자·특수문자 혼용)

--- src/main/java/com/mindpalace/MP_Backend/entity/MemberEntity.java
+++ src/main/java/com/mindpalace/MP_Backend/entity/MemberEntity.java
@@ -7,6 +7,8 @@ import lombok.Setter;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 @Setter
@@ -24,13 +26,18 @@ public class MemberEntity {
 
     @Column
     @NotBlank(message = "비밀번호는 필수 입력 값입니다")
+    @Size(min = 8, message = "비밀번호는 최소 8글자 이상이어야 합니다")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#$%^&+=!]).*$",
+            message = "비밀번호는 영문, 숫자, 특수문자를 혼용해야 합니다"
+    )
     private String memberPassword;
 
     @Column
     @NotBlank(message = "이름은 필수 입력 값입니다")
     private String memberName;
 
-    public static MemberEntity toMemberEntity(MemberDTO memberDTO){
+    public static MemberEntity toMemberEntity(MemberDTO memberDTO) {
         MemberEntity memberEntity = new MemberEntity();
         memberEntity.setMemberEmail(memberDTO.getMemberEmail());
         memberEntity.setMemberName(memberDTO.getMemberName());
@@ -38,7 +45,7 @@ public class MemberEntity {
         return memberEntity;
     }
 
-    public static MemberEntity toUpdateMemberEntity(MemberDTO memberDTO){
+    public static MemberEntity toUpdateMemberEntity(MemberDTO memberDTO) {
         MemberEntity memberEntity = new MemberEntity();
         memberEntity.setId(memberDTO.getId());
         memberEntity.setMemberEmail(memberDTO.getMemberEmail());
