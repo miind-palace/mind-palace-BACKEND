6ae969bd29fdf499b02b7d4522ef2709122d5350
reheat <reheat1540@gmail.com>
2023-10-15 18:21:47 +0900

    feat : API 이름 및 버전 정보 등 추가

--- src/main/java/com/mindpalace/MP_Backend/config/SwaggerConfig.java
+++ src/main/java/com/mindpalace/MP_Backend/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.mindpalace.MP_Backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -17,6 +19,18 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.mindpalace.MP_Backend"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        String title = "MindPalace API";
+        String version = "1.0.0";
+        String description = "MindPalace API Documents";
+        return new ApiInfoBuilder()
+                .title(title)
+                .version(version)
+                .description(description)
                 .build();
     }
 }
