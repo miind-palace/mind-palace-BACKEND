6bf2add54869a2c79944a0978938a98ec7f37217
reheat <reheat1540@gmail.com>
2023-10-15 21:26:48 +0900

    feat: Production Server도 테스트할 수 있도록 Swagger 설정 추가

--- src/main/java/com/mindpalace/MP_Backend/config/SwaggerConfig.java
+++ src/main/java/com/mindpalace/MP_Backend/config/SwaggerConfig.java
@@ -6,24 +6,33 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public Docket api() {
+        Server serverLocal = new Server("local", "http://localhost:8081", "for local usages", Collections.emptyList(), Collections.emptyList());
+        Server productionServer = new Server("real", "https://www.mind-palace.co.kr/", "for realServer", Collections.emptyList(), Collections.emptyList());
+
         return new Docket(DocumentationType.OAS_30)
+                .servers(serverLocal, productionServer)
+                .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.mindpalace.MP_Backend"))
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.mindpalace.MP_Backend")) //특정 패키지 경로를 API 문서화 함 : 1차 필터
+                .paths(PathSelectors.any())// apis 중 특정 path조건 API만 문서화함 : 2차 필터
                 .build()
-                .apiInfo(apiInfo());
+                .groupName("API 1.0.0")//group별 명칭 부여
+                .useDefaultResponseMessages(false); // 400,404,500 .. 표기를 ui에서 삭제한다.
+
+
     }
 
-    private ApiInfo apiInfo(){
+    private ApiInfo apiInfo() {
         String title = "MindPalace API";
         String version = "1.0.0";
         String description = "MindPalace API Documents";
---
+++ src/main/java/com/mindpalace/MP_Backend/config/Workaround.java
@@ -0,0 +1,34 @@
+package com.mindpalace.MP_Backend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.stereotype.Component;
+import springfox.documentation.oas.web.OpenApiTransformationContext;
+import springfox.documentation.oas.web.WebMvcOpenApiTransformationFilter;
+import springfox.documentation.spi.DocumentationType;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Component
+public class Workaround implements WebMvcOpenApiTransformationFilter {
+
+    @Override
+    public OpenAPI transform(OpenApiTransformationContext<HttpServletRequest> context) {
+        OpenAPI openApi = context.getSpecification();
+        Server localServer = new Server();
+        localServer.setDescription("local");
+        localServer.setUrl("http://localhost:8081");
+
+        Server testServer = new Server();
+        testServer.setDescription("production");
+        testServer.setUrl("https://www.mind-palace.co.kr/");
+        openApi.setServers(Arrays.asList(localServer, testServer));
+        return openApi;
+    }
+
+    @Override
+    public boolean supports(DocumentationType documentationType) {
+        return documentationType.equals(DocumentationType.OAS_30);
+    }
+}
