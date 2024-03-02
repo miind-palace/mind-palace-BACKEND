ee2e1432aaab109f60765d903388d904b2693593
Owon <chlehdnjs@gmail.com>
2024-03-02 18:30:11 +0900

    fix: add origin allowing options

--- src/main/java/com/mindpalace/MP_Backend/config/WebConfig.java
+++ src/main/java/com/mindpalace/MP_Backend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","https://mind-palace.co.kr","https://mindspalace.com/")
+                .allowedOrigins("http://localhost:3000","https://mind-palace.co.kr","https://mindspalace.com/","mind-palace-fe.vercel.app")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
